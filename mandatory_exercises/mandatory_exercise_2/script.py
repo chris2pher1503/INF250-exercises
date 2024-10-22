6c2d5cf74c716f13870db367b51becc8e2a746b6
chris2pher1503 <christopher150304@outlook.com>
2024-10-22 13:26:20 +0200

    problems removing holes

--- mandatory_exercises/mandatory_exercise_2/script.py
+++ mandatory_exercises/mandatory_exercise_2/script.py
@@ -4,6 +4,7 @@ import numpy as np
 
 ImgPath = 'IMG_2754_nonstop_alltogether.JPG'
 image = io.imread(ImgPath)
+image = image[200:3500 , 300:5500]
 plt.figure()
 plt.title("Original Image")
 plt.imshow(image)
@@ -13,21 +14,35 @@ plt.figure()
 plt.title("Gray-scale Image")
 plt.imshow(gray_image, cmap='gray')
 
-smoothed_image = filters.gaussian(gray_image, sigma=0.5)
-plt.figure()
-plt.title("smoothed image")
-plt.imshow(smoothed_image, cmap='gray')
-
-binary_image = smoothed_image > filters.threshold_otsu(smoothed_image)
+binary_image = gray_image > filters.threshold_otsu(gray_image)
 plt.figure()
 plt.title("Binary Image")
 plt.imshow(binary_image, cmap='gray')
 
+""" smoothed_image = filters.gaussian(binary_image, sigma=1)
+plt.figure()
+plt.title("smoothed image")
+plt.imshow(smoothed_image, cmap='gray')
+ """
+ 
+footprint = morphology.disk(20)
+""" binary_image = morphology.erosion(binary_image, footprint)
+binary_image = morphology.dilation(binary_image, footprint) """
 
-cleaned_image = morphology.remove_small_objects(binary_image, 5000)
+cleaned_image = morphology.closing(binary_image, footprint)
+plt.figure()
+plt.imshow(cleaned_image, cmap='gray')
+cleaned_image = morphology.opening(cleaned_image, footprint)
+
+
+
+""" cleaned_image = morphology.opening(binary_image, footprint) """
+
+""" cleaned_image = morphology.area_opening(binary_image, area_threshold=100)
 cleaned_image = morphology.remove_small_holes(cleaned_image, 5000)
+cleaned_image = morphology.remove_small_objects(cleaned_image, 10000)  """
 plt.figure()
 plt.title("Cleaned Binary Image")
-plt.imshow(cleaned_image, cmap='gray')
+plt.imshow(binary_image, cmap='gray')
 
 plt.show()
